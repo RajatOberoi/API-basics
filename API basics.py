26339055400fbadc2a21454b9c0f3e92e46f173f
RajatOberoi <41837453+RajatOberoi@users.noreply.github.com>
2018-09-05 02:16:50 +0530

    Delete API basics.py

--- API basics.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-response = requests.get("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json&key=#5935994c05/")
-a = json.loads(response.text)
-print(response)
-print(response.content)
-print(a["quoteText"])
